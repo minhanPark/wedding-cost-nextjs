eefd83afa3c842d83e0aee550a70eed46d743381
Running water <alsgks1218@naver.com>
2025-10-29 12:00:11 +0900

    shadcn에서 카드 컴포넌트 추가하고 헤더에 총 필요 금액 추가

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return <div className="">아베베베엔</div>;
+  return (
+    <div className="p-2">
+      <Card className="py-2">
+        <CardContent>
+          <CardDescription>총 필요 금액</CardDescription>
+          <CardTitle className="text-xl">₩0</CardTitle>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
